421ab96275393c50dab7d1688e3ad1967604f80a
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-12-27 16:40:10 +0400

    feat:post and put request

--- GameStore.api/Program.cs
+++ GameStore.api/Program.cs
@@ -1,5 +1,7 @@
 using GameStore.api.Entities;
 
+const string GetGameEndpointName = "GetGame";
+
 List<Game> games = new()
 {
    new Game(){
@@ -34,12 +36,34 @@ var app = builder.Build();
 app.MapGet("/games", () => games);
 app.MapGet("/games/{id}", (int id) =>
 {
-    Game? game = games.Find(game => game.Id == id);
-    if (game is null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(game);
+   Game? game = games.Find(game => game.Id == id);
+   if (game is null)
+   {
+      return Results.NotFound();
+   }
+   return Results.Ok(game);
+}).WithName(GetGameEndpointName);
+
+app.MapPost("/games", (Game game) =>
+{
+   game.Id = games.Max(game => game.Id) + 1;
+   games.Add(game);
+   return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
+
+app.MapPut("/games/{id}", (int id, Game updatedGame) =>
+{
+   Game? existingGame = games.Find(game => game.Id == id);
+   if (existingGame is null)
+   {
+      return Results.NotFound();
+   }
+   existingGame.Name = updatedGame.Name;
+   existingGame.Genre = updatedGame.Genre;
+   existingGame.Price = updatedGame.Price;
+   existingGame.ReleaseDate = updatedGame.ReleaseDate;
+   existingGame.ImageUri = updatedGame.ImageUri;
+   return Results.NoContent();
 });
 
 app.Run();
