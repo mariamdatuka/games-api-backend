f6d2de87168fe3da1abc2c261524f1f5284552b0
mariamdatuka <mariam.datukishvili.7@gmail.com>
2023-12-28 23:49:51 +0400

    feat:add repository patterns

--- GameStore.api/Endpoints/GamesEndpoints.cs
+++ GameStore.api/Endpoints/GamesEndpoints.cs
@@ -1,62 +1,32 @@
 using GameStore.api.Entities;
+using GameStore.api.Repositories;
 
 namespace GameStore.api.Endpoints;
 public static class GamesEndpoints
 {
     const string GetGameEndpointName = "GetGame";
 
-    static List<Game> games = new()
-{
-   new Game(){
-Id=1,
-Name="Street Fighte",
-Genre="Fighting",
-Price=19.99M,
-ReleaseDate=new DateTime(1991,2,1),
-ImageUri="https://placehold.co/100",
-},
-   new Game(){
-Id=2,
-Name="Fuck Life",
-Genre="Horror",
-Price=19.90M,
-ReleaseDate=new DateTime(1997,2,1),
-ImageUri="https://placehold.co/100",
-},
-   new Game(){
-Id=3,
-Name="Street Art",
-Genre="Drama",
-Price=29.99M,
-ReleaseDate=new DateTime(1999,2,1),
-ImageUri="https://placehold.co/100",
-}
-};
+
     public static RouteGroupBuilder MapGamesEndpoints(this IEndpointRouteBuilder routes)
     {
-
+        InMemGamesRepository repository = new();
         var group = routes.MapGroup("/games").WithParameterValidation();
-        group.MapGet("/", () => games);
+        group.MapGet("/", () => repository.GetAll());
         group.MapGet("/{id}", (int id) =>
         {
-            Game? game = games.Find(game => game.Id == id);
-            if (game is null)
-            {
-                return Results.NotFound();
-            }
-            return Results.Ok(game);
+            Game? game = repository.Get(id);
+            return game is not null ? Results.Ok(game) : Results.NotFound();
         }).WithName(GetGameEndpointName);
 
         group.MapPost("/", (Game game) =>
         {
-            game.Id = games.Max(game => game.Id) + 1;
-            games.Add(game);
+            repository.Create(game);
             return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
         });
 
         group.MapPut("/{id}", (int id, Game updatedGame) =>
         {
-            Game? existingGame = games.Find(game => game.Id == id);
+            Game? existingGame = repository.Get(id);
             if (existingGame is null)
             {
                 return Results.NotFound();
@@ -66,15 +36,17 @@ ImageUri="https://placehold.co/100",
             existingGame.Price = updatedGame.Price;
             existingGame.ReleaseDate = updatedGame.ReleaseDate;
             existingGame.ImageUri = updatedGame.ImageUri;
+
+            repository.Update(existingGame);
             return Results.NoContent();
         });
 
         group.MapDelete("/{id}", (int id) =>
         {
-            Game? game = games.Find(game => game.Id == id);
+            Game? game = repository.Get(id);
             if (game is not null)
             {
-                games.Remove(game);
+                repository.Delete(id);
             }
             return Results.NoContent();
         });
---
+++ GameStore.api/Repositories/InMemGamesRepository.cs
@@ -0,0 +1,60 @@
+using GameStore.api.Entities;
+
+namespace GameStore.api.Repositories;
+
+public class InMemGamesRepository
+{
+    private readonly List<Game> games = new()
+{
+   new Game(){
+Id=1,
+Name="Street Fighte",
+Genre="Fighting",
+Price=19.99M,
+ReleaseDate=new DateTime(1991,2,1),
+ImageUri="https://placehold.co/100",
+},
+   new Game(){
+Id=2,
+Name="Fuck Life",
+Genre="Horror",
+Price=19.90M,
+ReleaseDate=new DateTime(1997,2,1),
+ImageUri="https://placehold.co/100",
+},
+   new Game(){
+Id=3,
+Name="Street Art",
+Genre="Drama",
+Price=29.99M,
+ReleaseDate=new DateTime(1999,2,1),
+ImageUri="https://placehold.co/100",
+}
+};
+    public IEnumerable<Game> GetAll()
+    {
+        return games;
+    }
+
+    public Game? Get(int id)
+    {
+        return games.Find(game => game.Id == id);
+    }
+
+    public void Create(Game game)
+    {
+        game.Id = games.Max(game => game.Id) + 1;
+        games.Add(game);
+    }
+    public void Update(Game updatedGame)
+    {
+        var index = games.FindIndex(game => game.Id == updatedGame.Id);
+        games[index] = updatedGame;
+    }
+    public void Delete(int id)
+    {
+        var index = games.FindIndex(game => game.Id == id);
+        games.RemoveAt(index);
+
+    }
+}
